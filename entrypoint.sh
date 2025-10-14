e19f1fac846883baa124ed14f22eed559c69b55d
Ebil Abilli <ebilebilli3gmail.com>
2025-10-15 00:15:24 +0400

    Docker structure created for deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/env sh
+#!/bin/bash
 set -e
 
 echo "Starting API Gateway..."
+echo "PORT environment variable: ${PORT:-8080}"
 
 exec uvicorn gateway.main:app --host 0.0.0.0 --port ${PORT:-8080} --proxy-headers
