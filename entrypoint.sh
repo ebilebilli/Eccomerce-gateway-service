5d7cf818d85333080be256c30ed0429a5348892f
Ebil Abilli <ebilebilli3gmail.com>
2025-10-15 00:09:45 +0400

    Docker structure created for deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ set -e
 
 echo "Starting API Gateway..."
 
-exec uvicorn gateway.main:app --host 0.0.0.0 --port ${PORT} --proxy-headers
+exec uvicorn gateway.main:app --host 0.0.0.0 --port ${PORT:-8080} --proxy-headers
