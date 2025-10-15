b9f813e1a17f6bed52f8893d281487a6ca6c7041
Ebil Abilli <ebilebilli3gmail.com>
2025-10-15 10:30:05 +0400

    Docker structure created for deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 echo "Starting FastAPI entrypoint..."
 
 # Start FastAPI
-exec uvicorn src.main:app --host 0.0.0.0 --port ${PORT} --proxy-headers
+exec uvicorn gateway.main:app --host 0.0.0.0 --port ${PORT} --proxy-headers
