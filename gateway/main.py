aadfa9c57560b83eb8e0ead97dc0d46ad5c013a8
Ebil Abilli <ebilebilli3gmail.com>
2025-10-16 11:48:17 +0400

    some chnages made for test

--- gateway/main.py
+++ gateway/main.py
@@ -16,8 +16,8 @@ app.include_router(shop_cart_service.router)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # await register_shop_routes()
-    await register_shop_cart_routes()
+    await register_shop_routes()
+    # await register_shop_cart_routes()
     
     yield
 
